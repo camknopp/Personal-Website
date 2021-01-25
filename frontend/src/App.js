2699a6e72c287ff4d12a59bb50936f8e1105342d
camknopp <camknopp@gmail.com>
2021-01-25 09:41:53 -0500

    Added in Spotify & Youtube embeddings

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,48 +1,52 @@
 import React, { useState } from "react"
 import "./App.css"
-import { Instagram, Facebook, Youtube } from "react-feather"
 import MenuItem from "./components/MenuItem"
 import SocialButton from "./components/SocialButton"
+import InstagramEmbed from "react-instagram-embed"
+import ReactPlayer from "react-player"
+import SpotifyPlayer from 'react-spotify-player'
 
 function App() {
-
 	const [nameHovered, setNameHovered] = useState(false)
-	const [instagramHovered, setInstagramHovered] = useState(false)
-	const [facebookHovered, setFacebookHovered] = useState(false)
-	const [youtubeHovered, setYoutubeHovered] = useState(false)
 
 	return (
 		<div>
 			<div style={{ whiteSpace: "pre-wrap" }}>{` `}</div>
 			<header>
-				<div
-					class="main-title"
-					style={{ opacity: nameHovered ? 0.5 : 1 }}
-					onMouseOver={() => {
-						setNameHovered(true)
-					}}
-					onMouseOut={() => {
-						setNameHovered(false)
-					}}
-					cursor={nameHovered ? "pointer" : "cursor"}
-				>
-					Cam Knopp : Musician
-				</div>
-				
-        <SocialButton websiteName={"Youtube"} url={"https://www.youtube.com/channel/UCcwGXpKW4HmBtmRC5I7wFEg"} />
-        <SocialButton websiteName={"Instagram"} url={"https://www.instagram.com/camknopp/?hl=en"} />
-        <SocialButton websiteName={"Facebook"} url={"https://www.facebook.com/camknoppmusic/"} />
+				<div class="main-title">Cam Knopp : Musician</div>
+
+				<SocialButton
+					websiteName={"Youtube"}
+					url={"https://www.youtube.com/channel/UCcwGXpKW4HmBtmRC5I7wFEg"}
+				/>
+				<SocialButton
+					websiteName={"Instagram"}
+					url={"https://www.instagram.com/camknopp/?hl=en"}
+				/>
+				<SocialButton
+					websiteName={"Facebook"}
+					url={"https://www.facebook.com/camknoppmusic/"}
+				/>
 				<br></br>
 				<br></br>
 				<br></br>
 				<br></br>
 
-        <MenuItem url={"www.google.com"} name={"Home"} />
-        <MenuItem url={"www.google.com"} name={"Music"} />
-        <MenuItem url={"www.google.com"} name={"Merch"} />
-        <MenuItem url={"www.google.com"} name={"About"} />
+				<MenuItem url={"www.google.com"} name={"Home"} />
+				<MenuItem url={"www.google.com"} name={"Music"} />
+				<MenuItem url={"www.google.com"} name={"Merch"} />
+				<MenuItem url={"www.google.com"} name={"About"} />
 
+				<br></br>
+				<br></br>
+				<br></br>
 			</header>
+			<div>
+				<ReactPlayer url="https://www.youtube.com/watch?v=3fmqava7CxI&ab_channel=CamKnoppMusic" />
+			</div>
+      <div>
+        <SpotifyPlayer uri="https://open.spotify.com/track/7ItGXjb2MaHFwrWQcBphCA?si=j8LAki_pQFCh2G7d5iu74w" />
+      </div>
 		</div>
 	)
 }
